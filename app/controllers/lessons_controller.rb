e164c6b80d9e918d9ecd77fec38b49fd61df61e0
ShadiJam <shadijam00@gmail.com>
2017-02-09 17:50:48 -0600

    thumbnail renders

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,17 +1,19 @@
 class LessonsController < ApplicationController
 
     def index
-      @lesson = Lesson.all.where(published: true)
+      @video = Video.where(params[:lesson_id])
+      @lesson = Lesson.includes(:videos).all.where(published: true)
+
     end
 
 
     def show
-      @lesson = Lesson.find(params[:id])
+      @lesson = Lesson.includes(:videos).find(params[:id])
     end
 
     private
 
     def lesson_params
-    params.require(:lesson).permit(:image, :title, :description, :video_url, :published)
+    params.require(:lesson).permit(:image, :title, :description, :published, :videos)
   end
 end
